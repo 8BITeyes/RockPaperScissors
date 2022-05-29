e9d56c15950c975c97716e013c7586515b3d58be
RyanFrazin <RyanFrazin@Gmail.com>
2022-05-28 21:30:08 -0700

    Ryan Frazin May 28, 2022 9:30PM
    	Declare function playRound evaluated variables playerSelection and
    	computerSelection and determines if player wins or loses.
    
    	Add won and lost strings to indicate to player if won or lost in
    	console.

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log("poop");
+/* Create function that returns 'Rock', 'Paper', or 'Scissors' randomly from console */
+function computerPlay() {
+    const choices = ['rock', 'paper', 'scissors'];
+    return choices[Math.floor(Math.random() * choices.length)];
+}
+/* Create function that takes user input and declares if game is won */
+function playRound(playerSelection, computerSelection) {
+    const won = `You won! ${playerSelection} beats ${computerSelection}!`;
+    const lost = `You lost! ${computerSelection} beats ${playerSelection}!`;
+    
+    if (
+        (playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() === 'paper') ||
+        (playerSelection.toLowerCase() === 'paper' && computerSelection.toLowerCase() === 'scissors') ||
+        (playerSelection.toLowerCase() === 'scissors' && computerSelection.toLowerCase() === 'rock')) { 
+            return(lost);
+    } else if (
+        (playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() === 'scissors') ||
+        (playerSelection.toLowerCase() === 'paper' && computerSelection.toLowerCase() === 'rock') ||
+        (playerSelection.toLowerCase() === 'scissors' && computerSelection.toLowerCase() === 'paper')) {
+            return(won);
+    } else (playerSelection === computerSelection); {
+    return "Draw! Try again!";
+    }
+}
+
+const computerSelection = computerPlay();
+console.log(computerSelection);
+const playerSelection = "paper";
+console.log(playRound(playerSelection, computerSelection));
+/* Create function game(). Call */
+/* Declare playerSelection and computerSelection, make case insensitive */
